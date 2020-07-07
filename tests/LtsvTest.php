bc59ea84d847b120f02406df9c732ea73a5fa3f9
Kouhei Sano <apple.x.co@gmail.com>
2020-07-07 09:11:36 +0900

    chore: update QA

--- tests/LtsvTest.php
+++ tests/LtsvTest.php
@@ -1,12 +1,15 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Maison\Ltsv;
 
 use PHPUnit\Framework\TestCase;
 
 final class LtsvTest extends TestCase
 {
-    public function testClass() {
-        $this->assertSame(get_class(new \Maison\Ltsv\Ltsv()), 'Maison\Ltsv\Ltsv');
+    public function testClass(): void
+    {
+        $this->assertInstanceOf(Ltsv::class, new Ltsv());
     }
 }
